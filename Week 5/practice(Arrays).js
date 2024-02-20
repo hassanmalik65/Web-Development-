11a0849c7e6405dd4e3e1f44bdcba94b7ed0b76b
Hassan Iqbal <hassan.bscsses21@iba-suk.edu.pk>
2024-02-20 15:29:58 +0500

    practice

---
+++ Week 5/practice(Arrays).js
@@ -0,0 +1,17 @@
+let myArray = [1, 2, 3, 4, 5];
+let myArray2 = [100,200,300];
+//myArray.push(0);
+//myArray.pop();
+//myArray.shift();
+//myArray.unshift(100);
+//myArray3= myArray.concat(myArray2);
+// myArray3= myArray.slice(2);
+// myArray.splice(5, 2, 500);
+// myArray2=myArray.indexOf(6);
+// myArray2=myArray.lastIndexOf(4);
+// myArray2=myArray.includes(4);
+
+console.log(myArray2);
+
+
+ 
